8d319988bea0c259dc1a92a08fa652bb583c450e
Digvijay Singh <27089128+dnitros@users.noreply.github.com>
2024-07-11 02:09:54 +0530

    session 1 - refactor/format tf files

--- session-1_iac-and-terraform-intro/main.tf
+++ session-1_iac-and-terraform-intro/main.tf
@@ -1,9 +1,9 @@
-resource "aws_vpc" "iac-lab-exercise-vpc" {
+resource "aws_vpc" "iac-lab-tfm-vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
   instance_tenancy     = "default"
   tags = {
-    Name = "iac-lab-exercise:digvijay"
+    Name = "iac-lab-tfm:dnitros"
   }
 }
--- session-1_iac-and-terraform-intro/output.tf
+++ session-1_iac-and-terraform-intro/output.tf
@@ -1,4 +1,4 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value = aws_vpc.iac-lab-exercise-vpc.id
+  value       = aws_vpc.iac-lab-tfm-vpc.id
 }
