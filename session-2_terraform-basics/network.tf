ce969103d9d529fc69678d02555b2b2ac4a10b95
Digvijay Singh <27089128+dnitros@users.noreply.github.com>
2024-07-13 02:24:52 +0530

    session 2 - attach subnets to vpc

--- session-2_terraform-basics/network.tf
+++ session-2_terraform-basics/network.tf
@@ -7,3 +7,57 @@ resource "aws_vpc" "vpc" {
     Name = format("%s-vpc", var.prefix)
   }
 }
+
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet1_cidr
+  availability_zone = format("%sa", var.region)
+  tags = {
+    Name = format("%s-public-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet2_cidr
+  availability_zone = format("%sb", var.region)
+  tags = {
+    Name = format("%s-public-subnet-2", var.prefix)
+  }
+}
+
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet3_cidr
+  availability_zone = format("%sa", var.region)
+  tags = {
+    Name = format("%s-private-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet4_cidr
+  availability_zone = format("%sb", var.region)
+  tags = {
+    Name = format("%s-private-subnet-2", var.prefix)
+  }
+}
+
+resource "aws_subnet" "secure-subnet-1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet5_cidr
+  availability_zone = format("%sa", var.region)
+  tags = {
+    Name = format("%s-secure-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "secure-subnet-2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet6_cidr
+  availability_zone = format("%sb", var.region)
+  tags = {
+    Name = format("%s-secure-subnet-2", var.prefix)
+  }
+}
--- session-2_terraform-basics/outputs.tf
+++ session-2_terraform-basics/outputs.tf
@@ -2,3 +2,33 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = aws_vpc.vpc.id
 }
+
+output "public_subnet_1_id" {
+  description = "The ID of the VPC"
+  value       = aws_subnet.public-subnet-1.id
+}
+
+output "public_subnet_2_id" {
+  description = "The ID of the VPC"
+  value       = aws_subnet.public-subnet-2.id
+}
+
+output "private_subnet_1_id" {
+  description = "The ID of the VPC"
+  value       = aws_subnet.private-subnet-1.id
+}
+
+output "private_subnet_2_id" {
+  description = "The ID of the VPC"
+  value       = aws_subnet.private-subnet-2.id
+}
+
+output "secure_subnet_1_id" {
+  description = "The ID of the VPC"
+  value       = aws_subnet.secure-subnet-1.id
+}
+
+output "secure_subnet_2_id" {
+  description = "The ID of the VPC"
+  value       = aws_subnet.secure-subnet-2.id
+}
