9130bd70bb7b9fd53cfcc2ae045a5b17123a3a84
Digvijay Singh <27089128+dnitros@users.noreply.github.com>
2024-07-13 00:38:53 +0530

    session 1 - code reactor | add readme

--- session-1_iac-and-terraform-intro/main.tf
+++ session-1_iac-and-terraform-intro/main.tf
@@ -1,3 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.57.0"
+    }
+  }
+  required_version = ">= 1.9.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region to deploy resources in"
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "The CIDR block for the VPC"
+  default     = "192.168.0.0/25"
+}
+
 resource "aws_vpc" "iac-lab-tfm-vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_support   = true
@@ -6,4 +32,9 @@ resource "aws_vpc" "iac-lab-tfm-vpc" {
   tags = {
     Name = "iac-lab-tfm:dnitros"
   }
+}
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.iac-lab-tfm-vpc.id
 }
